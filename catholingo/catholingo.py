af4eb69ab8e307426da279607eeb7912d97faf50
jr <jr@jirouette.com>
2019-09-07 14:05:34 +0200

    Added database storage and sentence generator

--- catholingo/catholingo.py
+++ catholingo/catholingo.py
@@ -3,36 +3,53 @@
 
 import os
 import discord
+from sentence import Sentence, init as dbinit
 from voice import Voice
+import datetime
+
+WEBHOOKS = dict()
 
 class CathoLingo(discord.Client):
     COMMAND_PREFIX = "!"
     COMMENT_PREFIX = "-"
 
-    def __init__(self, commands=None, patterns=None, parsers=None):
+    def __init__(self, commands=None, patterns=None, on_message=None, on_edit=None, on_delete=None):
         super().__init__()
         self.commands = commands or dict()
         self.patterns = patterns or dict()
-        self.parsers = parsers or list()
-
-    async def get_user_by_id(self, ID):
-        ID = int(ID)
-        user = self.get_user(ID)
-        if not user:
-            user = await self.fetch_user(ID)
-        return user
+        self.on_message_parsers = on_message or list()
+        self.on_edit_parsers = on_edit or list()
+        self.on_delete_parsers = on_delete or list()
 
     async def on_ready(self):
-        print('Now connected as{0.user.name} ! '.format(self))
+        print('Now connected as {0.user.name}'.format(self))
         print('Ready. ')
 
+    async def send_as(self, message, channel, ID):
+        user = self.get_user(ID)
+        try:
+            if not user:
+                user = await self.fetch_user(ID)
+            URL = WEBHOOKS[channel.id]
+        except:
+            return await channel.send(message)
+        webhook = discord.Webhook.from_url(URL, adapter=discord.RequestsWebhookAdapter())
+        webhook.send(message, username=user.display_name + " (CathoLingo)", avatar_url=user.avatar_url)
+
     async def on_message(self, message):
+        message.content = message.content.replace("<@!", "<@")
         if message.content.startswith(self.COMMENT_PREFIX):
             return
         elif message.content.startswith(self.COMMAND_PREFIX):
             return await self.command(message)
         await self.pattern(message)
-        await self.parse(message)
+        await self.on_message_parse(message)
+
+    async def on_message_delete(self, message):
+        await self.on_delete_parse(message)
+
+    async def on_message_edit(self, before, after):
+        await self.on_edit_parse(before, after)
 
     async def command(self, message):
         cmd, *args = message.content.split()
@@ -45,16 +62,32 @@ class CathoLingo(discord.Client):
             if pattern in message.content:
                 await action(self, message)
 
-    async def parse(self, message):
-        for parser in self.parsers:
+    async def on_message_parse(self, message):
+        for parser in self.on_message_parsers:
             await parser(self, message)
 
+    async def on_edit_parse(self, before, after):
+        for parser in self.on_edit_parsers:
+            await parser(self, before, after)
+
+    async def on_delete_parse(self, message):
+        for parser in self.on_delete_parsers:
+            await parser(self, message)
 
 async def say(catholingo, message, *args, **__):
     await message.channel.send(" ".join(args))
 
 if __name__ == '__main__':
     print("Starting CathoLingo...")
+    dbinit()
+    print("Database initialized. ")
+    for key, value in os.environ.items():
+        if key.startswith("WEBHOOK_CHANNEL_"):
+            try:
+                WEBHOOKS[int(key.replace("WEBHOOK_CHANNEL_", ""))] = value
+            except:
+                pass
+    print("{0} webhook(s) configured. ".format(len(WEBHOOKS)))
     voice = Voice()
 
     commands = {
@@ -64,11 +97,19 @@ if __name__ == '__main__':
         'resume': voice.resume,
         'stop': voice.stop,
         'tts': voice.on_tts,
-        'say': say
+        'say': say,
+        'speak': Sentence.query,
+        'speakfor': Sentence.speakfor,
+        'speakfrom': Sentence.speakfrom,
+        'startwith': Sentence.startwith,
+        'endwith': Sentence.endwith,
     }
     patterns = {
         voice.YOUTUBE_PATTERN: voice.on_youtube,
     }
-    parsers = [voice.on_catholingo]
+    on_message = [voice.on_catholingo, Sentence.save]
+    on_edit = [Sentence.edit]
+    on_delete = [Sentence.delete]
 
-    CathoLingo(commands, patterns, parsers).run(os.environ.get('DISCORD_TOKEN'))
+    print("Connection. ")
+    CathoLingo(commands, patterns, on_message, on_edit, on_delete).run(os.environ.get('DISCORD_TOKEN'))
--- catholingo/sentence.py
+++ catholingo/sentence.py
@@ -1,12 +1,233 @@
 #!/usr/bin/python3
 #coding: utf8
 
+from peewee import (MySQLDatabase, Model, CharField, ForeignKeyField, TextField, BigIntegerField,
+                    DateTimeField, BooleanField, IntegerField, fn)
+from playhouse.db_url import connect as connect_url
+import datetime
+import os
+
+db = MySQLDatabase(
+    os.environ.get('DATABASE', 'catholingo'),
+    user=os.environ.get('DB_USER', 'catholingo'),
+    password=os.environ.get('DB_PASSWORD', ''),
+    host=os.environ.get('DB_HOST', 'mysql'),
+    charset='utf8mb4')
+
+class Word(Model):
+    class Meta:
+        database = db
+
+    message_id = BigIntegerField()
+    author_id = BigIntegerField()
+    channel_id = BigIntegerField()
+    guild_id = BigIntegerField()
+    datetime = DateTimeField(default=datetime.datetime.now)
+    author_username = CharField(max_length=50)
+    author_display_name = CharField(max_length=50)
+    previous_previous_word = CharField(null=True, max_length=500)
+    previous_word = CharField(null=True, max_length=500)
+    word = CharField(max_length=500)
+    next_word = CharField(null=True, max_length=500)
+    next_next_word = CharField(null=True, max_length=500)
+
+def init():
+    create_tables()
+
+def none_or(field, val):
+    return field == val if not (val is None) else field >> None
+
+def execute(sql):
+    return db.execute_sql(sql)
+
+def random():
+    return fn.Rand() if type(db) is MySQLDatabase else fn.Random()
+
+def connect():
+    if db.is_closed():
+        db.connect()
+
+def close():
+    if not db.is_closed():
+        db.close()
+
+def transaction():
+    return db.transaction()
+
+def create_tables():
+    connect()
+    with transaction():
+        db.create_tables([Word], safe=True)
+
+class connection(object):
+    def __enter__(self):
+        connect()
+    def __exit__(self, type, value, traceback):
+        close()
+
 class Sentence(object):
     @classmethod
-    def save(cls, message):
-        pass
+    async def save(cls, catholingo, message, no_save=False):
+        if not message.content:
+            return []
+        if message.content[0:2] not in ["!w", "!m"] and message.content[0] in [catholingo.COMMAND_PREFIX, catholingo.COMMENT_PREFIX]:
+            return []
+        if catholingo.user.name in message.author.name or "Jewvenden" in message.author.name:
+            return []
 
+        if catholingo.user in message.mentions or catholingo.user.name.lower() in message.content.lower():
+            await cls.query(catholingo, message)
+
+        text = message.content.split()
+        entries = []
+        for i, word in enumerate(text):
+            entry = dict(
+                message_id=message.id,
+                author_id=message.author.id,
+                channel_id=message.channel.id,
+                guild_id=message.channel.guild.id if hasattr(message.channel, "guild") else 0,
+                datetime=message.created_at,
+                author_username=message.author.name,
+                author_display_name=message.author.display_name,
+                previous_previous_word=text[i-2][:500] if i > 1 else None,
+                previous_word=text[i-1][:500] if i > 0 else None,
+                word=word[:500],
+                next_word=text[i+1][:500] if i < len(text)-1 else None,
+                next_next_word=text[i+2][:500] if i < len(text)-2 else None)
+            entries.append(entry)
+        if not no_save:
+            Word.insert_many(entries).execute()
+        return entries
 
     @classmethod
-    def query(cls):
-        pass
+    async def edit(cls, catholingo, before, after):
+        await cls.delete(catholingo, before)
+        await cls.save(catholingo, after)
+
+    @classmethod
+    async def delete(cls, catholingo, message):
+        Word.delete().where(Word.message_id == message.id).execute()
+
+    @staticmethod
+    def generator(conditions=None, ordered_conditions=None, maxlength=100):
+        if type(ordered_conditions) is not list:
+            ordered_conditions = [ordered_conditions] if ordered_conditions else []
+        pos = 0
+        payload = []
+        previous_previous_word = None
+        previous_word = None
+        word = True
+        with connection():
+            while word:
+                _cond = none_or(Word.previous_previous_word, previous_previous_word)
+                _cond = _cond & none_or(Word.previous_word, previous_word)
+                if conditions is not None:
+                    _cond = _cond & conditions
+                try:
+                    _cond = _cond & ordered_conditions[pos]
+                except IndexError:
+                    pass
+                pos += 1
+                word = Word.select().where(_cond).order_by(random()).first()
+                if word:
+                    payload.append(word.word)
+                    previous_previous_word = previous_word
+                    previous_word = word.word
+                    if word.next_word is None:
+                        break
+        return " ".join(payload) or "No result"
+
+    @staticmethod
+    def reverse_generator(conditions=None, ordered_conditions=None, maxlength=100):
+        if type(ordered_conditions) is not list:
+            ordered_conditions = [ordered_conditions] if ordered_conditions else []
+        pos = 0
+        payload = []
+        next_next_word = None
+        next_word = None
+        word = True
+        with connection():
+            while word:
+                _cond = none_or(Word.next_next_word, next_next_word)
+                _cond = _cond & none_or(Word.next_word, next_word)
+                if conditions is not None:
+                    _cond = _cond & conditions
+                try:
+                    _cond = _cond & ordered_conditions[pos]
+                except IndexError:
+                    pass
+                pos += 1
+                word = Word.select().where(_cond).order_by(random()).first()
+                if word:
+                    payload.append(word.word)
+                    next_next_word = next_word
+                    next_word = word.word
+                    if word.previous_word is None:
+                        break
+        payload.reverse()
+        return " ".join(payload) or "No result"
+
+    @classmethod
+    async def query(cls, catholingo, message, *_, **__):
+        await message.channel.trigger_typing()
+        await message.channel.send(cls.generator())
+
+    @classmethod
+    async def speakfor(cls, catholingo, message, *arg, **__):
+        if len(arg) < 1:
+            return
+
+        await message.channel.trigger_typing()
+        try:
+            if '<@' in arg[0]:
+                ID = int(arg[0].replace('<@', '').replace('!', '').replace('>', ''))
+                payload = cls.generator(Word.author_id == ID)
+            else:
+                ID = 0
+                username = " ".join(arg)
+                payload = cls.generator(Word.author_username == username | Word.author_display_name == username)
+        except:
+            return
+        if payload == "No result":
+            return await message.channel.send(payload)
+        await catholingo.send_as(payload, message.channel, ID)
+
+    @classmethod
+    async def speakfrom(cls, catholingo, message, *arg, **__):
+        if len(arg) < 1:
+            return
+
+        await message.channel.trigger_typing()
+        try:
+            ID = int(arg[0].replace('<#', '').replace('!', '').replace('>', ''))
+        except:
+            return
+        await message.channel.send(cls.generator(Word.channel_id == ID))
+
+    @classmethod
+    async def startwith(cls, catholingo, message, *arg, **__):
+        await message.channel.trigger_typing()
+        payload = cls.generator(ordered_conditions=[Word.word == word for word in arg])
+        await message.channel.send(payload)
+
+    @classmethod
+    async def endwith(cls, catholingo, message, *arg, **__):
+        await message.channel.trigger_typing()
+        payload = cls.reverse_generator(ordered_conditions=[Word.word == word for word in arg][::-1])
+        await message.channel.send(payload)
+
+    @classmethod
+    async def sync(cls, catholingo, message, *_, **__):
+        finished = False
+        channel = message.channel
+        previous_message = None
+        Word.delete().where(Word.channel_id == channel.id).execute()
+        while not finished:
+            finished = True
+            entries = []
+            async for message in channel.history(after=previous_message, oldest_first=True):
+                entries += await cls.save(catholingo, message, no_save=True)
+                previous_message = message
+                finished = False
+            Word.insert_many(entries).execute()
+        message.channel.send("Finished ! :3")
